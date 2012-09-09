e560851385ba5c27fe06def2b8e5656ada419349
Dorian Snyder <dastin1015@gmail.com>
2012-09-08 20:57:27 -0700

    camera: make it so this only builds on qcom hardware devices

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
 ifneq ($(USE_CAMERA_STUB),true)
+ifeq ($(strip $(BOARD_USES_QCOM_HARDWARE)), true)
+BUILD_LIBCAMERA:=true
+ifeq ($(BUILD_LIBCAMERA),true)
     # When zero we link against libmmcamera; when 1, we dlopen libmmcamera.
     DLOPEN_LIBMMCAMERA:=1
     ifneq ($(BUILD_TINY_ANDROID),true)
